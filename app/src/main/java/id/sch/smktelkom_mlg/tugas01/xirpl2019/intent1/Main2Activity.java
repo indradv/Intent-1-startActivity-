89223a1c249e09f761d2d54dd092127151ecc25c
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-10-09 13:08:28 +0700

    OnBackPressed Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent1/Main2Activity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent1/Main2Activity.java
@@ -2,12 +2,14 @@ package id.sch.smktelkom_mlg.tugas01.xirpl2019.intent1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
@@ -16,5 +18,15 @@ public class Main2Activity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+@Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    if (item.getItemId() == android.R.id.home){
+        onBackPressed();
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+}
 }
