86ea87c0e51c88379a15cf301516575de78b3056
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-10-09 13:48:05 +0700

    StartActivitypart2

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent1/Main2Activity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/intent1/Main2Activity.java
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+    String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR,0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
@@ -19,7 +23,11 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " lahir pada tahun"+ tahunLahir);
+        setTitle("Hasil");
     }
 @Override
     public boolean onOptionsItemSelected(MenuItem item){
